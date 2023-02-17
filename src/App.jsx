67a58a771e810143bae131df61191ce699cb0702
Alex <alexanderbergquist80@gmail.com>
2023-02-17 18:07:58 +0100

    spinning torus

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState, useRef } from "react"
+import { Canvas, useFrame } from "@react-three/fiber"
+import { OrbitControls } from '@react-three/drei'
+import { Physics } from '@react-three/cannon'
 
-function App() {
+import "./assets/style/style.css";
 
-  return (
-    <div className="App">
-    </div>
-  )
+const black = '#000000'
+
+const Torus = () => {
+	const myTorus = useRef()
+	useFrame(() => {
+		myTorus.current.rotation.x += 0.006;
+		myTorus.current.rotation.y += 0.001;
+		myTorus.current.rotation.z += 0.003;
+	})
+	return (
+		<mesh
+			ref={myTorus}
+			position={[15, 2, 10]}
+		>
+			<torusGeometry attach="geometry" args={[10, 3, 20, 100]}/>
+			<meshStandardMaterial attach="material" color={black}/>
+		</mesh>
+	)
 }
 
-export default App
+
+function App() {
+	return (
+		<Canvas>
+			<OrbitControls />
+			<ambientLight intensity={0.5} />
+			<Physics>
+				<Torus />
+			</Physics>
+		</Canvas>
+	)
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// import { Canvas } from '@react-three/fiber'
+// import "./assets/style/style.css";
 
+
+import App from './App'
+
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+// 	<Canvas>
+
+// 	</Canvas>
+// )
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
